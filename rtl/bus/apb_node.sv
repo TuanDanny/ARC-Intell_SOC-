36fcf18a102d54387cb5d0b07c9d017d6759c137
TuanDanny <88261316+TuanDanny@users.noreply.github.com>
2026-03-19 09:39:13 +0700

    Update apb_node.sv

--- rtl/bus/apb_node.sv
+++ rtl/bus/apb_node.sv
@@ -1,22 +1,3 @@
-// Copyright 2016 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-
-// Engineer:       Davide Rossi - davide.rossi@unibo.it
-//                 Florian Zaruba - zarubaf@iis.ee.ethz.ch
-// Design Name:    APB Bus
-// Module Name:    apb_node
-// Project Name:   PULP/Kerbin
-// Language:       SystemVerilog
-//
-// Description:    This component implements a configurable APB node
-
 module apb_node #(
         parameter int unsigned NB_MASTER      = 8,
         parameter int unsigned APB_DATA_WIDTH = 32,
