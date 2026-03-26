7476efc44cc85f6144ece84021b65f7dfb586f67
TuanDanny <88261316+TuanDanny@users.noreply.github.com>
2026-03-26 13:44:37 +0700

    Update top_soc.sv

--- rtl/top_soc.sv
+++ rtl/top_soc.sv
@@ -67,8 +67,8 @@ module top_soc (
     assign start_addr[2] = `GPIO_BASE_ADDR;  assign end_addr[2] = `GPIO_BASE_ADDR  + 32'h0FFF;
     assign start_addr[3] = `UART_BASE_ADDR;  assign end_addr[3] = `UART_BASE_ADDR  + 32'h0FFF;
     assign start_addr[4] = `TIMER_BASE_ADDR; assign end_addr[4] = `TIMER_BASE_ADDR + 32'h0FFF;
-    assign start_addr[5] = 32'h0000_5000;    assign end_addr[5] = 32'h0000_5FFF; // WDT
-    assign start_addr[6] = 32'h0000_6000;    assign end_addr[6] = 32'h0000_6FFF; // BIST
+    assign start_addr[5] = `WATCHDOG_BASE_ADDR;    assign end_addr[5] = `WATCHDOG_BASE_ADDR + 32'h0FFF; // WDT
+    assign start_addr[6] = `BIST_BASE_ADDR;    assign end_addr[6] = `BIST_BASE_ADDR + 32'h0FFF; // BIST
 
     // Master Interface from CPU
     APB_BUS apb_cpu_master();
